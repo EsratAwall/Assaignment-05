dce8394c6b596b1e06ec6e8749cfb24ecfb12898
CompleteTechSolution <protech.thecompletetechsolution@gmail.com>
2024-09-23 18:26:52 +0600

    navbar added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// sticky navbar
+window.addEventListener('scroll', function () {
+    var navbar = document.getElementById('nav');
+    var tobNavColor = document.getElementById('navbar-color');
+    if (window.scrollY > 100) {
+        navbar.style.backdropFilter = 'blur(10px)';
+        tobNavColor.style.backdropFilter = 'blur(10px)';
+        tobNavColor.style.backgroundColor = 'transparent';
+        
+    }else{
+        navbar.style.backdropFilter = 'blur(0px)';
+        tobNavColor.style.backgroundColor = '#f4f4f5';
+        tobNavColor.style.backdropFilter = 'blur(0px)';
+    }
+})
